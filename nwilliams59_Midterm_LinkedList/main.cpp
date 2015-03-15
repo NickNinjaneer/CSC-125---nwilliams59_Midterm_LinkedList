afc51260eb898b4b2ef3173db4d32a6abaf377de
Nick Williams <SoundsCustom@gmail.com>
2015-03-15 16:15:52 -0500

    Initial Commit

---
+++ nwilliams59_Midterm_LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nwilliams59_Midterm_LinkedList
+//
+//  Created by Nick Williams on 3/15/15.
+//  Copyright (c) 2015 Ninjaneer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
